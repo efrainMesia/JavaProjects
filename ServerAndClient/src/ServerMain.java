da5ae2a8c028e1c4d7b23be00c673e7e98e39807
Efrain Mesia <64331443+efrainMesia@users.noreply.github.com>
2022-02-26 22:04:40 +0200

    Add files via upload

---
+++ ServerAndClient/src/ServerMain.java
@@ -0,0 +1,23 @@
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ServerMain extends Application{ 
+	
+	public void start(Stage stage) throws Exception{ 
+		Parent root = (Parent) FXMLLoader.load(getClass().getResource("ServerMain.fxml")); 
+		Scene scene = new Scene(root); 
+		stage.setTitle("ServerMain"); 
+		stage.setScene(scene); 
+		stage.show(); 
+	} 
+	
+	public static void main(String[] args) { 
+		launch(args); 
+		System.out.println();
+	} 
+}
